fabe8d70c664395a59eaf02109c62245e1041c26
sarahogilvie <ogil0039@algonquinlive.com>
2017-01-23 13:11:40 -0500

    Update looper.js

--- js/looper.js
+++ js/looper.js
@@ -6,8 +6,8 @@ while (!letter) {
     letter = prompt("Enter a character.");
 }
 
-for(i = 0; i < 11; i += 1) {
-    for(k = 0; k < i; k += 1) {
+for(i = 0; i < 10; i += 1) {
+    for(k = 0; k <= i; k += 1) {
         document.write(letter); 
     }
     document.write("<br>"); 
